ce709a6b89c33937826915e9263c3d6d7c7b29e5
Benjamin de Vaublanc <benjamin.devaublanc@popsiit.net>
2015-05-13 17:54:19 +0200

    create gallery

--- src/Dai/PublicBundle/Controller/PublicController.php
+++ src/Dai/PublicBundle/Controller/PublicController.php
@@ -3,7 +3,6 @@
 namespace Dai\PublicBundle\Controller;
 
 use Dai\PortfolioBundle\Entity\Work;
-use Dai\PortfolioBundle\Entity\Image;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,10 +11,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 class PublicController extends Controller
 {
 
-    public function indexAction()
+    public function indexAction($page)
     {
-    
 
+    	if ($page < 1) {
+    	    throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+    	}
+
+    	$nbPerPage = 5;
+
+    	$works = $this->getDoctrine()
+    	    ->getManager()
+    	    ->getRepository('DaiPortfolioBundle:Work')
+    	    ->getWorks($page, $nbPerPage)
+    	;
+
+    	
+    	$nbPages = ceil(count($works)/$nbPerPage);
+    	
+    	if ($page > $nbPages) {
+    	    throw $this->createNotFoundException("La page ".$page." n'existe pas.");
+    	}
+
+    	return $this->render('DaiPublicBundle:Public:index.html.twig', array(
+    	    'works' => $works,
+    	    'nbPages' => $nbPages,
+    	    'page' => $page
+    	));
+    
         return $this->render('DaiPublicBundle:Public:index.html.twig', array());
 
     }
